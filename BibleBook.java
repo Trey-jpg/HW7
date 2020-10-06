7530d83ac74cc90c769b4886bbe8ce2fb24b1f74
Trey-jpg <56291093+Trey-jpg@users.noreply.github.com>
2020-10-06 02:26:08 -0500

    Create BibleBook.java

---
+++ BibleBook.java
@@ -0,0 +1,60 @@
+public class BibleBook extends Object
+{
+	private String name;
+	private int number_of_chapters;
+	private String summary;
+	
+	public BibleBook(String name, int number_of_chapters, String summary)
+	{
+		this.name = name;
+		this.number_of_chapters = number_of_chapters;
+		this.summary = summary;
+	}
+	
+	public BibleBook(String s)
+	{
+		String[] parts = s.split(":");
+		this.name = parts[0];
+		this.number_of_chapters = Integer.parseInt(parts[1]);
+		this.summary = parts[2];
+	}
+	
+	int addNums(int a, int b)
+	{
+		return a + b;
+	}
+	
+	static int addNums2(int a, int b)
+	{
+		return a + b;
+	}
+	
+	public int getNumber_of_chapters() 
+	{
+		return number_of_chapters;
+	}
+
+	public String getSummary() 
+	{
+		return summary;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String newName)
+	{
+		if(newName.length() >= 5)
+		{
+			this.name = newName;
+		}
+	}
+	
+	public void display()
+	{
+		System.out.println("Book Name: " + this.name + 
+				"(" + this.number_of_chapters + " chapters) - " + this.summary);
+	}
+}
